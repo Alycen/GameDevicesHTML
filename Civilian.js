62337a0fcbeefa2914e5dbc6f070fc3b0ec1630a
Alison <alsunnid@gmail.com>
2014-12-10 10:38:43 +0000

    added Move() code to Civilian

--- Civilian.js
+++ Civilian.js
@@ -3,8 +3,8 @@ function Civilian(xPos, yPos) {
 	this.y = yPos;
 	//texture - not a touch area
 	this.scale = 1;
-	this.width = 20;
-	this.height = 30;
+	this.width = 23;	//width of tempNPC
+	this.height = 49;	//height of tempNPC
 	this.isMarked = false;
 	this.isShot = false;
 
@@ -16,7 +16,35 @@ function Civilian(xPos, yPos) {
 }
 
 Civilian.prototype.Move = function() {
+	var timer = 30 * 3;	//assuming 30 FPS
+	var UP=0, DOWN=1, LEFT=2, RIGHT=3, STOP=5;
+	var verticleSpeed=1, horizontalSpeed=2;
+	//random number generator between UP && STOP
+	var dir; //= rand(UP, STOP);
 
+	for ( ; timer >= 0 ; timer -- ) {
+		if ( timer == 0 ) {
+			timer = 30 * 3;
+		}
+
+		if ( dir == UP ) {
+			this.y -= verticleSpeed;
+		}
+		else if ( dir == DOWN ) {
+			this.y += verticleSpeed;
+		}
+		else if ( dir == LEFT ) {
+			this.x -= horizontalSpeed;
+		}
+		else if ( dir == RIGHT ) {
+			this.x += horizontalSpeed;
+		}
+		else {	// dir == STOP
+			//do nothing;
+			//this.x += 0;
+			//this.y += 0;
+		}
+	}
 }
 
 // Marked() 
